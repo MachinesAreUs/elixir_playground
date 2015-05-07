0b18b1dd2a70bdcd866724cd5621eb7127f9da1c
Agustin Ramos Fonseca <zentimental.software@gmail.com>
2015-05-07 16:18:38 -0500

    Started using ShouldI extension to ExUnit

--- katas/mix.exs
+++ katas/mix.exs
@@ -25,6 +25,6 @@ defmodule ZenSoft.Katas.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:shouldi, only: :test}]
   end
 end
--- katas/test/make_x_test.exs
+++ katas/test/make_x_test.exs
@@ -1,30 +1,31 @@
-defmodule ZenSoft.KatasTest do
-  use ExUnit.Case
+defmodule ZenSoft.Katas.MakeXTest do
   import ZenSoft.Katas.MakeX
+  use ExUnit.Case
+  use ShouldI
 
-  test "size 1" do
+  should "return 1" do
     assert make_x(1) == [['x']]
   end
 
-  test "size 2" do
+  should "size 2" do
     assert make_x(2) == [['x','x'],
                          ['x','x']]
   end
 
-  test "size 3" do
+  should "size 3" do
     assert make_x(3) == [['x',' ','x'],
                          [' ','x',' '],
                          ['x',' ','x']]
   end
 
-  test "size 4" do
+  should "size 4" do
     assert make_x(4) == [['x',' ',' ','x'],
                          [' ','x','x',' '],
                          [' ','x','x',' '],
                          ['x',' ',' ','x']]
   end
 
-  test "size 5" do
+  should "size 5" do
     assert make_x(5) == [['x',' ',' ',' ','x'],
                          [' ','x',' ','x',' '],
                          [' ',' ','x',' ',' '],
@@ -32,7 +33,7 @@ defmodule ZenSoft.KatasTest do
                          ['x',' ',' ',' ','x']]
   end
 
-  test "size 5 as strings" do
+  should "size 5 as strings" do
     assert make_x_str(5) == ["x   x",
                              " x x ",
                              "  x  ",
