9d6a45ec3a1f56f6f6b8156c7d69bca61ce03f03
Agustin Ramos Fonseca <zentimental.software@gmail.com>
2015-08-14 11:23:21 -0500

    Added bucket supervisor to the application supervisor

--- kv/lib/kv/supervisor.ex
+++ kv/lib/kv/supervisor.ex
@@ -5,15 +5,17 @@ defmodule KV.Supervisor do
     Supervisor.start_link __MODULE__, :ok
   end
 
-  @manager_name KV.EventManager
-  @registry_name KV.Registry
-  @logger_name KV.LogForwarder
+  @manager_name           KV.EventManager
+  @registry_name          KV.Registry
+  @bucket_supervisor_name KV.Bucket.Supervisor
+  @logger_name            KV.LogForwarder
 
   def init(:ok) do
     children = [
-      worker(GenEvent, [[name: @manager_name]]),
-      worker(KV.Registry, [@manager_name, [name: @registry_name]]),
-      worker(KV.LogForwarder, [[name: @logger_name]])
+      worker(GenEvent,             [[name: @manager_name]]),
+      worker(KV.Bucket.Supervisor, [[name: @bucket_supervisor_name]]),
+      worker(KV.Registry,          [@manager_name, @bucket_supervisor_name, [name: @registry_name]]),
+      worker(KV.LogForwarder,      [[name: @logger_name]])
     ]
 
     supervise children, strategy: :one_for_one
