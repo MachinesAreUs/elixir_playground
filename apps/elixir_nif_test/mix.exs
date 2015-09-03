4bcc57c9fd2942fa03a304bdc0b1d44776d51b3d
Agustin Ramos Fonseca <zentimental.software@gmail.com>
2015-09-03 15:46:10 -0500

    moved generation of .so to a Makefile

--- apps/elixir_nif_test/mix.exs
+++ apps/elixir_nif_test/mix.exs
@@ -7,9 +7,15 @@ defmodule ElixirNifTest.Mixfile do
      deps_path: "../../deps",
      lockfile: "../../mix.lock",
      elixir: "~> 1.0",
+     compilers: [:make, :elixir, :app],
+     aliases: aliases,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
+  end
+
+  defp aliases do
+    [clean: ["clean", "clean.make"]]
   end
 
   # Configuration for the OTP application
@@ -19,16 +25,31 @@ defmodule ElixirNifTest.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
   end
+
 end
+
+# Make file tasks
+
+defmodule Mix.Tasks.Compile.Make do
+  @shortdoc "Compiles helper in src"
+ 
+  def run(_) do
+    {result, _error_code} = System.cmd("make", [], stderr_to_stdout: true)
+    Mix.shell.info result
+    :ok
+  end
+end
+ 
+defmodule Mix.Tasks.Clean.Make do
+  @shortdoc "Cleans helper in src"
+ 
+  def run(_) do
+    {result, _error_code} = System.cmd("make", ['clean'], stderr_to_stdout: true)
+    Mix.shell.info result
+    :ok
+  end
+end
+
